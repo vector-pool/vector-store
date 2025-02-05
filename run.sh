47cc0208db6e7aa07935142f9bfe6ec4c1e950fe
macminix111 <damian78jerdewse@hotmail.com>
2025-02-05 19:03:42 +0000

    complete auto-update

--- run.sh
+++ run.sh
@@ -10,42 +10,30 @@ version="__version__"
 
 old_args=$@
 
-# Check if pm2 is installed
 if ! command -v pm2 &> /dev/null
 then
     echo "pm2 could not be found. To install see: https://pm2.keymetrics.io/docs/usage/quick-start/"
     exit 1
 fi
 
-# Checks if $1 is smaller than $2
-# If $1 is smaller than or equal to $2, then true.
-# else false.
 version_less_than_or_equal() {
     [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
 }
 
-# Checks if $1 is smaller than $2
-# If $1 is smaller than $2, then true.
-# else false.
 version_less_than() {
     [ "$1" = "$2" ] && return 1 || version_less_than_or_equal $1 $2
 }
 
-# Returns the difference between
-# two versions as a numerical value.
 get_version_difference() {
     local tag1="$1"
     local tag2="$2"
 
-    # Extract the version numbers from the tags
     local version1=$(echo "$tag1" | sed 's/v//')
     local version2=$(echo "$tag2" | sed 's/v//')
 
-    # Split the version numbers into an array
     IFS='.' read -ra version1_arr <<< "$version1"
     IFS='.' read -ra version2_arr <<< "$version2"
 
-    # Calculate the numerical difference
     local diff=0
     for i in "${!version1_arr[@]}"; do
         local num1=${version1_arr[$i]}
@@ -109,13 +97,11 @@ check_variable_value_on_github() {
     local url="https://api.github.com/repos/$repo/contents/$file_path?ref=$branch"
     local response=$(curl -s "$url")
 
-    # Check if the response contains an error message
     if [[ $response =~ "message" ]]; then
         echo "Error: Failed to retrieve file contents from GitHub."
         return 1
     fi
 
-    # Extract the content from the response
     local content=$(echo "$response" | tr -d '\n' | jq -r '.content')
 
     if [[ "$content" == "null" ]]; then
@@ -123,10 +109,8 @@ check_variable_value_on_github() {
         return 1
     fi
 
-    # Decode the Base64-encoded content
     local decoded_content=$(echo "$content" | base64 --decode)
 
-    # Extract the variable value from the content
     local variable_value=$(echo "$decoded_content" | grep "$variable_name" | awk -F '=' '{print $2}' | tr -d ' ')
 
     if [[ -z "$variable_value" ]]; then
@@ -140,37 +124,30 @@ check_variable_value_on_github() {
 strip_quotes() {
     local input="$1"
 
-    # Remove leading and trailing quotes using parameter expansion
     local stripped="${input#\"}"
     stripped="${stripped%\"}"
 
     echo "$stripped"
 }
 
-# Loop through all command line arguments
 while [[ $# -gt 0 ]]; do
   arg="$1"
 
-  # Check if the argument starts with a hyphen (flag)
   if [[ "$arg" == -* ]]; then
-    # Check if the argument has a value
     if [[ $# -gt 1 && "$2" != -* ]]; then
           if [[ "$arg" == "--script" ]]; then
             script="$2";
             shift 2
         else
-            # Add '=' sign between flag and value
             args+=("'$arg'");
             args+=("'$2'");
             shift 2
         fi
     else
-      # Add '=True' for flags with no value
       args+=("'$arg'");
       shift
     fi
   else
-    # Argument is not a flag, add it as it is
     args+=("'$arg '");
     shift
   fi
@@ -178,7 +155,6 @@ done
 
 echo "Received delay input: $delay seconds."
 
-# Check if script argument was provided
 if [[ -z "$script" ]]; then
     echo "The --script argument is required."
     exit 1
@@ -188,27 +164,20 @@ branch=$(git branch --show-current)            # get current branch.
 echo watching branch: $branch
 echo pm2 process name: $proc_name
 
-# Get the current version locally.
 current_version=$(read_version_value)
 
-# Check if script is already running with pm2
 if pm2 status | grep -q $proc_name; then
     echo "The script is already running with pm2. Stopping and restarting..."
     pm2 delete $proc_name
 fi
 
-# Check if script is already running with pm2
 
-# Run the Python script with the arguments using pm2
 echo "Running $script with the following pm2 config:"
 
-# Join the arguments with commas using printf
 joined_args=$(printf "%s," "${args[@]}")
 
-# Remove the trailing comma
 joined_args=${joined_args%,}
 
-# Create the pm2 config file
 echo "module.exports = {
   apps : [{
     name   : '$proc_name',
@@ -220,57 +189,41 @@ echo "module.exports = {
   }]
 }" > app.config.js
 
-# Print configuration to be used
 cat app.config.js
 
 pm2 start app.config.js
 
-# Check if packages are installed.
 check_package_installed "jq"
 if [ "$?" -eq 1 ]; then
-    # Add this at the beginning of the script, before the while loop
     last_restart_time=$(date +%s)
     restart_interval=$((30 * 3600))  # 30 hours in seconds
 
     while true; do
-        # Get current time
         current_time=$(date +%s)
         time_since_last_restart=$((current_time - last_restart_time))
 
-        # First ensure that this is a git installation
         if [ -d "./.git" ]; then
 
-            # check value on github remotely
             latest_version=$(check_variable_value_on_github "vector-pool/vector-store" "vectornet/__init__.py" "__version__ " "$branch")
 
-            # If the file has been updated
             if version_less_than $current_version $latest_version; then
                 echo "latest version $latest_version"
                 echo "current version $current_version"
-                # Pull latest changes
-                # Failed git pull will return a non-zero output
                 sleep $delay # Apply the delay
                 
                 if git pull origin $branch; then
-                    # latest_version is newer than current_version, should download and reinstall.
                     echo "New version published. Updating the local copy."
 
-                    # Install latest changes just in case.
                     pip install -e .
 
-                    # # Run the Python script with the arguments using pm2
-                    # TODO (shib): Remove this pm2 del in the next spec version update.
                     echo "Restarting PM2 process"
                     pm2 restart $proc_name
                     
-                    # Update current version:
                     current_version=$(read_version_value)
                     echo ""
 
-                    # Update last restart time
                     last_restart_time=$current_time
 
-                    # Restart autorun script
                     echo "Restarting script..."
                     ./$(basename $0) $old_args && exit
                 else
@@ -282,7 +235,6 @@ if [ "$?" -eq 1 ]; then
                 echo "**Skipping update **"
                 echo "$current_version is the same as or more than $latest_version. You are likely running locally."
 
-                # Check if 30 hours have passed since last restart
                 if [ $time_since_last_restart -ge $restart_interval ]; then
                     echo "30 hours passed. Performing periodic PM2 restart..."
                     pm2 restart $proc_name
@@ -295,10 +247,7 @@ if [ "$?" -eq 1 ]; then
             echo "The installation does not appear to be done through Git. Please install from source at https://github.com/opentensor/validators and rerun this script."
         fi
         
-        # Wait about 30 minutes
-        # This should be plenty of time for validators to catch up
-        # and should prevent any rate limitations by GitHub.
-        sleep 60
+        sleep 1800
     done
 else
     echo "Missing package 'jq'. Please install it for your system first."
