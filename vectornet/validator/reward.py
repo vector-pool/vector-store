41af7aae6fc8c23ce16c44ff63f46de75e2c2148
macminix111 <damian78jerdewse@hotmail.com>
2025-02-06 18:48:30 +0000

    chore: update reward func

--- vectornet/validator/reward.py
+++ vectornet/validator/reward.py
@@ -20,42 +20,37 @@ def reward(score: float, weight: float) -> float:
     return miner_reward
 
 async def get_rewards(
-    create_request_zero_score: float,
-    update_request_zero_scores: List[float],
-    delete_request_zero_score: float,
+    create_score: float,
+    update_scores: List[float],
+    delete_score: float,
     read_score: float,
     weight: float,
 ) -> np.ndarray:
     """Calculate rewards based on request scores and weight.
 
     Args:
-        create_request_zero_score (float): Score for create requests.
-        update_request_zero_score (float): Score for update requests.
-        delete_request_zero_score (float): Score for delete requests.
+        create_score (float): Score for create requests.
+        update_scores (List[float]): Scores for update requests.
+        delete_score (float): Score for delete requests.
         read_score (float): Score for read operations.
         weight (float): Weight to apply in the reward calculation.
 
     Returns:
         np.ndarray: The calculated miner reward as a NumPy array.
     """
-    initial_create_score, initial_delete_score, initial_update_score = 1.0, 1.0, 1.0
-    
-    zero_scores = [
-        create_request_zero_score,
-        delete_request_zero_score,
-    ]
-    
-    if create_request_zero_score == 0:
-        initial_create_score = 0.5
-        
-    if delete_request_zero_score == 0:
-        initial_delete_score = initial_delete_score * 0.5
-    
-    # if any(score == 0 for score in update_request_zero_scores):
-    #     initial_update_score = 0.5
-    for score in update_request_zero_scores:
+    create_multiplier = 0.5 if create_score == 0 else 1.0
+    delete_multiplier = 0.5 if delete_score == 0 else 1.0
+    update_multiplier = 1.0
+
+    for score in update_scores:
         if score == 0:
-            initial_update_score = initial_update_score * 0.3
-    read_score = read_score ** 5
-    miner_reward = reward(initial_create_score * initial_delete_score * initial_update_score * read_score, weight)
+            update_multiplier *= 0.5
+
+    adjusted_read_score = read_score ** 5
+
+    total_score = (
+        create_multiplier * delete_multiplier * update_multiplier * adjusted_read_score
+    )
+    miner_reward = reward(total_score, weight)
+
     return np.array(miner_reward)
